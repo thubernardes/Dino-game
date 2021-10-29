dbed46c68fa34df84fdfb4ba5e875349576f57fd
thubernardes <gallo.arthur@yahoo.com.br>
2021-10-29 15:03:38 -0300

    Finalizando o projeto, ajustando a função Jump limitando-a o sobe e desce, adicionando novas funcionalidades como a do Cacto com random, adicionando uma animação no CSS para o backgroud deslizar da direita para a esquerda gerando sensação de movimento e adicionando as imagens para o melhor visual.

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 const dino = document.querySelector('.dino');
+const background = document.querySelector('.background');
+let position = 0;
+
+let isJumping = false;
 
 function handleKeyUp(event) {
-    if (event.keyCode == 32)
-    jump();
+    if (event.keyCode == 32) {
+        if (!isJumping) {
+            jump();
+        }
+    }
 }
 
 function jump () {
-    let position = 0;
+
+    isJumping = true;
 
     let upInterval = setInterval (() => {
         if (position >= 150) {
@@ -16,6 +24,7 @@ function jump () {
             let downInterval = setInterval(() => {
                 if (position <= 0) {
                     clearInterval(downInterval);
+                    isJumping = false;
                 } else {
                   position -= 20;
                   dino.style.bottom = position + 'px';        
@@ -29,4 +38,33 @@ function jump () {
     }, 20);
 }
 
+function createCactus() {
+    const cactus = document.createElement('div');
+    let cactusPosition = 1000;
+    let randomTime = Math.random() * 6000;
+
+    cactus.classList.add('cactus');
+    cactus.style.left = 1000 + 'px';
+    background.appendChild(cactus);
+
+    let leftInterval = setInterval (() => {
+        if (cactusPosition < -60) {
+            clearInterval(leftInterval);
+            background.removeChild(cactus);
+        } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60) {
+            //Game over
+            clearInterval(leftInterval);
+            document.body.innerHTML = '<h1 class="game-over">Fim de jogo</h1>'
+            } else {
+            cactusPosition -= 10;
+            cactus.style.left = cactusPosition + 'px';
+        }
+    }, 20)
+     
+    setTimeout(createCactus, randomTime);
+}
+
+createCactus();
+
 document.addEventListener('keyup', handleKeyUp);
+ 
